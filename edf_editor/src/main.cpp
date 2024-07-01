90fe993995376db8477a42c925ca22a3f1dbea79
Manuel <manuelcabrerizo5@gmail.com>
2024-07-01 17:36:06 -0300

    add editor project

---
+++ edf_editor/src/main.cpp
@@ -0,0 +1,149 @@
+#include <stdio.h>
+
+#include <imgui.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_sdlrenderer2.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include <edf_common.h>
+
+static SDL_Window   *g_window;
+static SDL_Renderer *g_renderer;
+static bool g_running;
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
+struct Input {
+};
+
+
+i32 main(void) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    g_window = SDL_CreateWindow("EDF Editor",
+                                SDL_WINDOWPOS_UNDEFINED,
+                                SDL_WINDOWPOS_UNDEFINED,
+                                WINDOW_WIDTH, WINDOW_HEIGHT, 
+                                SDL_WINDOW_RESIZABLE|SDL_WINDOW_SHOWN);
+    g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
+
+    SDL_Texture *back_buffer = SDL_CreateTexture(g_renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET,
+                                                 WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    SDL_Texture *rocks_full = IMG_LoadTexture(g_renderer, "../assets/rock_full.png");
+    SDL_Texture *rocks_corner = IMG_LoadTexture(g_renderer, "../assets/rocks_corner.png");
+    SDL_Texture *rocks_flat = IMG_LoadTexture(g_renderer, "../assets/rocks_flat.png");
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL2_InitForSDLRenderer(g_window, g_renderer);
+    ImGui_ImplSDLRenderer2_Init(g_renderer);
+
+    g_running = true;
+    while(g_running) {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);            
+            if(event.type == SDL_QUIT) {
+                g_running = false;
+            }
+        }
+
+        SDL_SetRenderTarget(g_renderer, back_buffer);
+        SDL_SetRenderDrawColor(g_renderer, 180, 200, 180, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(g_renderer);
+        SDL_Rect dst;
+        dst.w = 100;
+        dst.h = 100;
+        for(i32 y = 0; y < WINDOW_HEIGHT/100; y++) {
+            for(i32 x = 0; x < WINDOW_WIDTH/100; x++) {
+                dst.x = x * 102;
+                dst.y = y * 102;
+                SDL_RenderCopyEx(g_renderer, rocks_full, 0, &dst, 0, 0, SDL_FLIP_NONE);
+            }
+        }
+        SDL_SetRenderTarget(g_renderer, 0);
+
+        ImGui_ImplSDLRenderer2_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        // Start ImGui Docking
+        ImGuiWindowFlags windowFlags;
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        windowFlags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+        windowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+        static bool condition = true;
+        ImGui::Begin("Dockspace demo", &condition, windowFlags);
+        ImGui::PopStyleVar();
+        ImGui::PopStyleVar();
+
+        ImGui::DockSpace(ImGui::GetID("Dockspace"));
+
+        // draw the back buffer
+        ImGui::Begin("Game Viewport");
+        ImVec4 border_col = ImGui::GetStyleColorVec4(ImGuiCol_Border);
+        ImGui::Image(back_buffer, ImVec2(WINDOW_WIDTH, WINDOW_HEIGHT), ImVec2(0, 0), ImVec2(1, 1), ImVec4(1, 1, 1, 1), border_col);
+        ImGui::End();
+
+        // TODO: do imgui demo
+        static bool show_demo = true; 
+        ImGui::ShowDemoWindow(&show_demo);
+
+       // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+
+        ImGui::End();
+        ImGui::Render();
+        ImGuiIO& io = ImGui::GetIO();
+        SDL_RenderSetScale(g_renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), g_renderer);
+
+        SDL_RenderPresent(g_renderer); 
+    }
+
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(g_renderer);
+    SDL_DestroyWindow(g_window);
+    SDL_Quit();
+
+    return(0);
+};
